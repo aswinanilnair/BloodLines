cad81faa0c4925aae2ac5819d2ce86bd9a039f82
aswinanilnair <aswinanilnair@gmail.com>
2020-04-25 16:41:26 +0530

    nothing

--- bank/models.py
+++ bank/models.py
@@ -3,7 +3,6 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class UserProfileInfo(models.Model):
-
     user = models.OneToOneField(User,on_delete=models.CASCADE)
 
     # add any additional attributes
