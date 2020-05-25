8f84a9be0b954525663f8c73d8a34df19b6f149c
aswinanilnair <aswinanilnair@gmail.com>
2020-05-25 08:34:26 +0530

    updated

--- bank/api/urls.py
+++ bank/api/urls.py
@@ -3,8 +3,7 @@ from bank.api.views import (
                 api_detail_blog_view,
                 api_add_blog_view,
                 api_detail_request_view,
-                api_add_request_view,
-                registration_view,)
+                api_add_request_view,)
 
 
 app_name = 'bank'
@@ -14,5 +13,4 @@ urlpatterns = [
     path('blog/create',api_add_blog_view,name="blog-create"),
     path('requests',api_detail_request_view,name="request-detail"),
     path('requests/new',api_add_request_view,name="request-new"),
-    path('register',registration_view,name="registration"),
 ]
